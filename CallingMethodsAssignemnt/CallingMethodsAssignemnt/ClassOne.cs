9c2b8ea74441187373acf0440c9c0ac40137c5a6
JBowersock <jbowersock22@hotmail.com>
2022-06-01 21:18:26 -0400

    Calling Methods Assignment (Setup)

---
+++ CallingMethodsAssignemnt/CallingMethodsAssignemnt/ClassOne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignemnt
+{
+    public class ClassOne //Class.
+    {
+        public int Addition() //Method
+        {
+             return userChoice + 12; //Returns calculation.
+        }
+
+        public int Subtract() //Method
+        {
+            return userChoice - 8; //Returns calculation.
+        }
+
+        public int Multiply() //Method
+        {
+            return userChoice * 3; //Returns calculation.
+        }
+    }
+    //public { get; set; } 
+}
---
+++ CallingMethodsAssignemnt/CallingMethodsAssignemnt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignemnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like us to do math operations on?"); //Prints string.
+            int userChoice = Convert.ToInt32(Console.ReadLine()); //Requests user input.
+
+            Console.WriteLine(userChoice + "+12="); //Prints string.
+            Console.WriteLine(Addition); //Prints result.
+
+            Console.WriteLine(userChoice + "-8="); //Prints string.
+            Console.WriteLine(Subtract); //Prints result.
+
+            Console.WriteLine(userChoice + "x3="); //Prints string.
+            Console.WriteLine(Multiply); //Prints result.
+        }
+    }
+}
