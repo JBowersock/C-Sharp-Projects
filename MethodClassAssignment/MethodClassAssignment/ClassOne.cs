d55a137f403950f4a63b4658be0138ec9323941a
JBowersock <jbowersock22@hotmail.com>
2022-06-02 13:13:52 -0400

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/ClassOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class ClassOne
+    {
+        public int methodOne(int userInput1, int userInput2)
+        {
+            int mathOperation = userInput1 + 12;
+
+            return userInput2;
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassOne assignment = new ClassOne(); //Instantiating the created 'ClassOne' class.
+
+            Console.WriteLine("Enter first number:"); //Prints string.
+            int userInput1 = Convert.ToInt32(Console.ReadLine()); //Requests 1st user input.
+
+                Console.WriteLine(); //Line Break.
+
+            Console.WriteLine("Enter second number:"); //Prints string.
+            int userInput2 = Convert.ToInt32(Console.ReadLine()); //Requests 2nd user input.
+
+                Console.WriteLine(); //Line Break.
+
+            Console.WriteLine("Results:"); //Prints string.
+            Console.WriteLine(userInput2); //Prints second user input.
+
+            Console.ReadLine(); //Allows window to remain open until user closes.
+        }
+    }
+}
