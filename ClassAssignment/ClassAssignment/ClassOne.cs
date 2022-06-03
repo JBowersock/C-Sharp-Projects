4ff88d5f45e66138e7f5a05a370a5cbc7fd3001c
JBowersock <jbowersock22@hotmail.com>
2022-06-03 11:56:08 -0400

    Class Assignment

---
+++ ClassAssignment/ClassAssignment/ClassOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class ClassOne
+    {
+        //Step 1 + 6: (Void Method) + (Declare a Method Static)
+        public static void methodOne(int userInput) //Property is 'public'. Data Type is 'int'. Name is 'methodOne'. Parameters are 'int userInput'.
+        {
+            int divideByTwo = userInput / 2; //Assigning math operation to a variable.
+            return;
+        }
+
+        //Step 4. (Method with Output Parameters)
+        public int methodTwo(out int num) => num = 22; //Property is 'public'. Data Type is 'int'. Name is 'methodTwo'.
+
+        //Step 5. (Overload Method)
+        public int methodThree(int a, int b) //Property is 'public'. Data Type is 'int'. Name is 'methodThree'. Parameters are 'int a, int b'.
+        {
+            int sum = a + b; //Assigning math operation to a variable.
+            return sum; //Return result.
+        }
+        public int methodThree(int a, int b, int c) //Property is 'public'. Data Type is 'int'. Name is 'methodThree'. Parameters are 'int a, int b, int c'.
+        {
+            int sum = a + b + c; //Assigning math operation to a variable.
+            return sum; //Return result.
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2.
+            ClassOne instantiation = new ClassOne(); //Instantiating the created 'ClassOne' class.
+
+            //Step 3.
+            Console.WriteLine("Enter a number:"); //Prints string.
+            int userInput = Convert.ToInt32(Console.ReadLine()); //Requests user input, then prints their input.
+
+            Console.WriteLine("Here is your number divided by two:"); //Prints string.
+            int result = instantiation.methodOne(userInput);
+            Console.WriteLine(result); //Prints result.
+
+            ////Step 4.
+            //int outputParameters;
+            //methodTwo(out outputParameters);
+            //Console.WriteLine(outputParameters); //Prints result.
+
+            //Step 5.
+            int sum1 = instantiation.methodThree(1, 2);
+            Console.WriteLine("The sum of the two values: " + sum1); //Prints string and result.
+
+            int sum2 = instantiation.methodThree(1, 2, 3);
+            Console.WriteLine("The sum of the three values: " + sum2); //Prints string and result.
+        }
+    }
+}
