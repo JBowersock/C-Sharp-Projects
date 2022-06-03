97ab65c5e9044e6dee5a681d06098615aedbfc06
JBowersock <jbowersock22@hotmail.com>
2022-06-03 13:20:25 -0400

    Methods and Objects Assignment

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Employee : Person //Inhereting from the class 'Person'.
+    {
+        public List<string> FirstName { get; set; }
+        public List<string> LastName { get; set; }
+
+        public int Id = 20; //property
+    }
+}
---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person(string firstName, string lastName) //made public.
+    {
+        public void sayName()
+        {
+            Console.WriteLine("Name:[full name]");
+        }
+    }
+}
