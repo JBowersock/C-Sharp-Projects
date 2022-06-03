0d4fc44573f3d36ddec4db7cc809bcc893c49b88
JBowersock <jbowersock22@hotmail.com>
2022-06-03 09:32:52 -0400

    Method Assignment (Finished)

--- MethodAssignment/MethodAssignment/ClassOne.cs
+++ MethodAssignment/MethodAssignment/ClassOne.cs
@@ -8,7 +8,8 @@ namespace MethodAssignment
 {
     public class ClassOne
     {
-        public int methodOne(int userChoice1, int userChoice2)
+        public int methodOne(int userChoice1, int userChoice2 = 0) 
+        //By inserting '= 0' here, it will utilize a default value of 0 for the second parameter (userChoice2). So, if the user doesnt enter a number the procedure will continue.
         {
             return userChoice1 + userChoice2;
         }
--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -12,26 +12,25 @@ namespace MethodAssignment
         {
             ClassOne assignment = new ClassOne(); //Instantiating the created 'ClassOne' class.
 
+
             Console.WriteLine("Enter first number:"); //Prints string.
             int userChoice1 = Convert.ToInt32(Console.ReadLine()); //Requests 1st user input.
-            Console.WriteLine(); //Line Break.
 
-            Console.WriteLine("Enter second number (optional):"); //Prints string.
-            string userChoice2 = Console.ReadLine(); //Requests 2nd user input.
-            int num = -1;
-            Console.WriteLine(); //Line Break.
-            if (!int.TryParse(userChoice2, out num))
+
+            try
             {
-                Console.WriteLine("Your entries combined equal:"); //Prints string.
-                Console.WriteLine(userChoice1); //Prints result.
+                Console.WriteLine("Enter second number (optional):"); //Prints string.
+                int userChoice2 = Convert.ToInt32(Console.ReadLine()); //Requests 2nd user input.
+                int result = assignment.methodOne(userChoice1, userChoice2);//Integer variable. Calls instantiated method. Parameter 'int userChoice1 and userChoice2'.
+                Console.WriteLine("Result is: " + result); //Prints string and result,
             }
-            else
+            catch (Exception)
             {
-                Console.WriteLine("Your entries combined equal:"); //Prints string.
-                int x = Int32.Parse(userChoice2); //Converting 'userChoice2' to integer for calculation below.
-                var result = userChoice1 + x; //Assigning calculation to a varaible.
-                Console.WriteLine(result); //Prints result.
+                int result = assignment.methodOne(userChoice1); //Integer variable. Calls instantiated method. Parameter 'int userChoice1'.
+                Console.WriteLine("Result is: " + result); //Prints string and result.
             }
+
+
             Console.ReadLine(); //Allows window to remain open until user closes.
         }
     }
