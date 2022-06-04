9dfcf5b43d43eb8f46bbe6ab67190b1962471fe1
JBowersock <jbowersock22@hotmail.com>
2022-06-03 20:04:44 -0400

    Polymorphism

---
+++ PolymorphismAssignment/PolymorphismAssignment/IQuittable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    //Step 1.
+    public interface IQuittable
+    {
+        public void Quit()
+        {
+            Console.Write("This is a sentence.");
+        }
+    }
+}
