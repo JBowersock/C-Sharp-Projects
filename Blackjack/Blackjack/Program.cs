06603726f9552421feb25175364b6a09c3aa5a0d
JBowersock <jbowersock22@hotmail.com>
2022-06-04 18:00:37 -0400

    Lambda

--- Blackjack/Blackjack/Program.cs
+++ Blackjack/Blackjack/Program.cs
@@ -10,16 +10,23 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
-            
+            //Card card1 = new Card();
+            //Card card2 = card1;
+            //card1.Face = Face.Eight;
+            //card2.Face = Face.King;
 
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
-
-            Console.WriteLine(card1.Face);
+            //Console.WriteLine(card1.Face);
 
             //Deck deck = new Deck(); //Instanitated an object called deck and assigned it to the variable;
+
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
+
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+            //List<int> numberList = new List<int>() { 1, 2, 3, 545, 342, 23 };
+            //int sum = numberList.Sum();
+            //Console.WriteLine(sum);
+
             //deck.Shuffle(3); //Shuffles deck 3x.
 
             //foreach (Card card in deck.Cards) //For Loop
@@ -27,6 +34,9 @@ namespace Blackjack
             //    Console.WriteLine(card.Face + " of " + card.Suit); //Prints string.
             //}
             //Console.WriteLine("Total Cards: " + deck.Cards.Count); //Prints total amount of cards in deck. (Result: 52).
+
+
+
             Console.ReadLine(); //Allows window to remain open until user closes.
         }
     }
